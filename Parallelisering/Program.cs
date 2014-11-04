82c00db6f047d0317f6231bfd2d2808f325e58f1
Johanna Axelsson <johannaaxelsson90@gmail.com>
2014-11-04 22:07:34 +0100

    parallel.for and foreach examples added

--- Parallelisering/Program.cs
+++ Parallelisering/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Parallelisering
 {
@@ -8,12 +10,40 @@ namespace Parallelisering
         {
             var quicksort = new Quicksort();
             var plinq = new Plinq();
-            for (int i = 0; i < 5; i++)
-            {
-                //plinq.PlinqQuery();
-                quicksort.QuicksortExample();
-            }
+            quicksort.QuicksortExample();
+            //plinq.PlinqQuery();
+            //ForExample();
+            //ForEachExample();
             //Console.ReadKey();
+        }
+
+
+        public static void ForExample()
+        {
+            const int size = 20;
+            var result = new int[size];
+            Parallel.For(0, size, i =>
+            {
+                result[i] = i*i;
+                Console.WriteLine(result[i]);
+            });
+            Console.WriteLine("Completed ForExample");
+        }
+
+        public static void ForEachExample()
+        {
+            var list = Enumerable.Range(0, 20);
+            var counterLock = new object();
+            var counter = 0;
+            Parallel.ForEach(list, i =>
+            {
+                lock (counterLock)
+                {
+                    counter++;
+                }
+                Console.WriteLine(i);
+            });
+            Console.WriteLine("Completed ForEachExample, counter: "+counter);
         }
     }
 }
--- Parallelisering/Quicksort.cs
+++ Parallelisering/Quicksort.cs
@@ -8,31 +8,21 @@ namespace Parallelisering
     class Quicksort
     {
         private const int Size = 10000000;
-        private const int Processors = 8;
         private static readonly int ThreadLimit = Environment.ProcessorCount * 2;
         volatile int _activeThreads = 0;
+        private const Boolean RunSerial = false;
 
         public void QuicksortExample()
         {
-            var list = InitList();
-            var timer = new Stopwatch();
-
-            timer.Start();
-            RecursiveQuicksort(list, 0, list.Length - 1, Processors);
-            timer.Stop();
-
-            Console.WriteLine("Time " + timer.ElapsedMilliseconds+" processorcount: "+Processors+" or threadLimit: "+ThreadLimit);
-        }
-
-        private static int[] InitList()
-        {
-            var list = new int[Size];
-            var random = new Random();
-            for (var i = 0; i < Size; i++)
+            for (int i = 0; i < 5; i++)
             {
-                list[i] = random.Next(Size);
+                var list = InitList();
+                var timer = new Stopwatch();
+                timer.Start();
+                RecursiveQuicksort(list, 0, list.Length - 1, ThreadLimit);
+                timer.Stop();
+                Console.WriteLine("Time " + timer.ElapsedMilliseconds + ", running serial:"+RunSerial+", ThreadLimit: " + ThreadLimit);
             }
-            return list;
         }
 
         public void RecursiveQuicksort(int[] array, int left, int right, int maxDepth)
@@ -52,28 +42,27 @@ namespace Parallelisering
                     SwapElements(array, last, current);
                 }
             }
-
             SwapElements(array, left, last);
-            if (maxDepth < 1)
-            //if (_activeThreads > ThreadLimit)
+
+            //if (maxDepth < 1 || RunSerial )
+            if (_activeThreads > ThreadLimit || RunSerial )
             {
                 RecursiveQuicksort(array, left, last - 1, maxDepth);
                 RecursiveQuicksort(array, last + 1, right, maxDepth);
             }
             else
             {
-                
+                /*
                 --maxDepth;
                 Parallel.Invoke(
                 () => RecursiveQuicksort(array, left, last - 1, maxDepth),
                 () => RecursiveQuicksort(array, last + 1, right, maxDepth));
-                /*
+                */
                 Interlocked.Increment(ref _activeThreads);
                 Parallel.Invoke(
                     () => RecursiveQuicksort(array, left, last - 1, maxDepth),
                     () => RecursiveQuicksort(array, last + 1, right, maxDepth));
                 Interlocked.Decrement(ref _activeThreads);
-                 */
             }
         }
 
@@ -82,6 +71,17 @@ namespace Parallelisering
             var temp = array[i];
             array[i] = array[j];
             array[j] = temp;
+        }
+
+        private static int[] InitList()
+        {
+            var list = new int[Size];
+            var random = new Random();
+            for (var i = 0; i < Size; i++)
+            {
+                list[i] = random.Next(Size);
+            }
+            return list;
         }
     }
 }
